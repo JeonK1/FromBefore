a59c65b43dfc72c9488284c921d81d8a8c36d672
xemic <kan5016@naver.com>
2020-07-14 00:50:01 +0900

    android file creation

---
+++ FromBefore/app/src/main/java/com/example/frombefore/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.frombefore
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
