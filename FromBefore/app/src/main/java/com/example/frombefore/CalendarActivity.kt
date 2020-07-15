e5034221645907225288b43ba43338f7dfdf8b17
JeonJaewon <wksk04515@naver.com>
2020-07-15 21:09:44 +0900

    여러가지함

--- FromBefore/app/src/main/java/com/example/frombefore/CalendarActivity.kt
+++ FromBefore/app/src/main/java/com/example/frombefore/CalendarActivity.kt
@@ -4,23 +4,14 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_init_message.*
-import java.util.Calendar
 
-class InitMessageActivity : AppCompatActivity() {
+class CalendarActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_init_message)
+        setContentView(R.layout.activity_calendar)
         nextBtn.setOnClickListener {
-            val received = intent
             val i = Intent(this, CalendarActivity::class.java)
-
-            i.putExtra("finalMessage", finalMessageInput.text)
-            i.putExtra("year", received.extras?.getInt("year"))
-            i.putExtra("month", received.extras?.getInt("month"))
-            i.putExtra("dayOfMonth", received.extras?.getInt("dayOfMonth"))
             startActivity(i)
         }
     }
-
-
 }
--- FromBefore/app/src/main/java/com/example/frombefore/InitDdayActivity.kt
+++ FromBefore/app/src/main/java/com/example/frombefore/InitDdayActivity.kt
@@ -4,45 +4,49 @@ import android.app.DatePickerDialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.DatePicker
+import android.view.View
+import android.widget.AdapterView
 import kotlinx.android.synthetic.main.activity_init_dday.*
 import java.text.SimpleDateFormat
 import java.util.*
 
 class InitDdayActivity : AppCompatActivity() {
+    lateinit var pickedCalendar: Calendar
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_init_dday)
-//        val listener = DatePickerDialog.OnDateSetListener { DatePicker(this), yy, mm, dd ->
-//            textView2.text = String.format("%d-%d-%d", yy,mm+1,dd)
-//        }
-//        DatePickerDialog.OnDateSetListener(){
-//
-//        }
-//        button2.setOnClickListener{
-//            DatePickerDialog(this,listener, 2020, 7, 15)
-//        }
-
         var cal = Calendar.getInstance()
         val mDateListener = DatePickerDialog.OnDateSetListener { datePicker, year, monthOfYear, dayOfMonth ->
             cal.set(Calendar.YEAR, year)
             cal.set(Calendar.MONTH, monthOfYear)
             cal.set(Calendar.DAY_OF_MONTH, dayOfMonth)
-
+            pickedCalendar = cal
             val myFormat = "yyyy.MM.dd"
             val sdf = SimpleDateFormat(myFormat, Locale.KOREA)
-            textView2.text = sdf.format(cal.time)
+            debug.text = sdf.format(cal.time)
         }
 
-        button2.setOnClickListener {
+        ddayBtn.setOnClickListener {
             DatePickerDialog(this, mDateListener,
                 cal.get(Calendar.YEAR),
                 cal.get(Calendar.MONTH),
                 cal.get(Calendar.DAY_OF_MONTH)).show()
         }
-        button3.setOnClickListener {
+        nextBtn.setOnClickListener {
             val i = Intent(this, InitMessageActivity::class.java)
+            i.putExtra("year", cal.get(Calendar.YEAR))
+            i.putExtra("month", cal.get(Calendar.MONTH))
+            i.putExtra("dayOfMonth", cal.get(Calendar.DAY_OF_MONTH))
             startActivity(i)
+        }
+
+        subject_spinner.onItemSelectedListener = object:AdapterView.OnItemSelectedListener{
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+            }
+
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, pos: Int, p3: Long) {
+                debug2.text = subject_spinner.getItemAtPosition(pos).toString()
+            }
         }
     }
 
