ac08f0d627061afdf92a613a725e4f5c21ed08d8
JeonJaewon <wksk04515@naver.com>
2020-07-17 02:34:58 +0900

    add input validation

--- FromBefore/app/src/main/java/com/example/frombefore/InitDdayActivity.kt
+++ FromBefore/app/src/main/java/com/example/frombefore/InitDdayActivity.kt
@@ -1,6 +1,8 @@
 package com.example.frombefore
 
+import android.app.AlertDialog
 import android.app.DatePickerDialog
+import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
@@ -17,42 +19,61 @@ class InitDdayActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_init_dday)
+
+        // 캘린더 초기화
         var cal = Calendar.getInstance()
-        val mDateListener = DatePickerDialog.OnDateSetListener { datePicker, year, monthOfYear, dayOfMonth ->
-            cal.set(Calendar.YEAR, year)
-            cal.set(Calendar.MONTH, monthOfYear)
-            cal.set(Calendar.DAY_OF_MONTH, dayOfMonth)
-            pickedCalendar = cal
-            val myFormat = "yyyy.MM.dd"
-            val sdf = SimpleDateFormat(myFormat, Locale.KOREA)
+        val mDateListener =
+            DatePickerDialog.OnDateSetListener { datePicker, year, monthOfYear, dayOfMonth ->
+                cal.set(Calendar.YEAR, year)
+                cal.set(Calendar.MONTH, monthOfYear)
+                cal.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+                pickedCalendar = cal
+                val myFormat = "yyyy.MM.dd"
+                val sdf = SimpleDateFormat(myFormat, Locale.KOREA)
 //            debug.text = sdf.format(cal.time)
-            val tmp = Calendar.getInstance()
-            val now_day = tmp.timeInMillis //현재 시간
-            val event_day = pickedCalendar.timeInMillis //목표일에 대한 시간
-            val d_day = (event_day - now_day) / (60 * 60 * 24 * 1000)
-            ddayBtn.text = (d_day+1).toString() + "일"
-        }
+                val tmp = Calendar.getInstance()
+                val now_day = tmp.timeInMillis //현재 시간
+                val event_day = pickedCalendar.timeInMillis //목표일에 대한 시간
+                val d_day = (event_day - now_day) / (60 * 60 * 24 * 1000)
+                ddayBtn.text = (d_day + 1).toString() + "일"
+            }
 
         ddayBtn.setOnClickListener {
-            DatePickerDialog(this, mDateListener,
+            DatePickerDialog(
+                this, mDateListener,
                 cal.get(Calendar.YEAR),
                 cal.get(Calendar.MONTH),
-                cal.get(Calendar.DAY_OF_MONTH)).show()
-        }
-        nextBtn.setOnClickListener {
-            val i = Intent(this, InitMessageActivity::class.java)
-            i.putExtra("year", cal.get(Calendar.YEAR))
-            i.putExtra("month", cal.get(Calendar.MONTH))
-            i.putExtra("dayOfMonth", cal.get(Calendar.DAY_OF_MONTH))
-            startActivity(i)
+                cal.get(Calendar.DAY_OF_MONTH)
+            ).show()
         }
 
+        // 다음 버튼 초기화
+        nextBtn.setOnClickListener {
+            if (!(::pickedCalendar.isInitialized)) { //초기화 되어 있는지 확인
+                val builder = AlertDialog.Builder(this)
+                builder.setMessage("목표 날짜를 선택해 주세요.")
+                builder.setPositiveButton(
+                    "OK",
+                    DialogInterface.OnClickListener { dialogInterface, id -> }) //아무것도 안하는데 OK만들어주고 싶어서 넣음
+                builder.create()
+                builder.show()
+            } else {
+                val i = Intent(this, InitMessageActivity::class.java)
+                i.putExtra("year", cal.get(Calendar.YEAR))
+                i.putExtra("month", cal.get(Calendar.MONTH))
+                i.putExtra("dayOfMonth", cal.get(Calendar.DAY_OF_MONTH))
+                i.putExtra("d_day", ddayBtn.text)
+                startActivity(i)
+            }
+        }
+
+        // 공부 spinner 초기화
         val subjectList = arrayOf("대학교 진학", "자격증 시험", "공무원 시험", "취직")
         val spinnerAdapter = ArrayAdapter(this, R.layout.spinner_item, subjectList)
         spinnerAdapter.setDropDownViewResource(R.layout.spinner_item)
         subject_spinner.adapter = spinnerAdapter
         subject_spinner.setSelection(0)
-        subject_spinner.onItemSelectedListener = object:AdapterView.OnItemSelectedListener{
+        subject_spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onNothingSelected(p0: AdapterView<*>?) {
             }
 
--- FromBefore/app/src/main/java/com/example/frombefore/InitMessageActivity.kt
+++ FromBefore/app/src/main/java/com/example/frombefore/InitMessageActivity.kt
@@ -1,5 +1,7 @@
 package com.example.frombefore
 
+import android.app.AlertDialog
+import android.content.DialogInterface
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -10,20 +12,37 @@ class InitMessageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_init_message)
+        val tmp = desc.text
+        desc.text= intent.extras?.get("d_day").toString() + tmp
         nextBtn.setOnClickListener {
-            val received = intent
-            val i = Intent(this, CalendarActivity::class.java)
-            val cal = Calendar.getInstance()
-            cal.set(Calendar.YEAR, received.extras!!.getInt("year"))
-            cal.set(Calendar.MONTH, received.extras!!.getInt("month"))
-            cal.set(Calendar.DAY_OF_MONTH, received.extras!!.getInt("dayOfMonth"))
-            var userInfo = UserInfo(goalDate = cal, d_day = 10, maxCombo = 0, finalMessage = finalMessageInput.text.toString())
-            i.putExtra("userInfo", userInfo)
-            i.putExtra("finalMessage", finalMessageInput.text)
-            i.putExtra("year", received.extras?.getInt("year"))
-            i.putExtra("month", received.extras?.getInt("month"))
-            i.putExtra("dayOfMonth", received.extras?.getInt("dayOfMonth"))
-            startActivity(i)
+            if(finalMessageInput.text.toString() == ""){
+                val builder = AlertDialog.Builder(this)
+                builder.setMessage("목표를 이룬 나에게 메세지를 작성 해 주세요.")
+                builder.setPositiveButton(
+                    "OK",
+                    DialogInterface.OnClickListener { dialogInterface, id -> }) //아무것도 안하는데 OK만들어주고 싶어서 넣음
+                builder.create()
+                builder.show()
+            }else{
+                // 이 액티비티가 받은 인텐트의 내용을 또 보내줘야함
+                val received = intent
+                val i = Intent(this, CalendarActivity::class.java)
+                val cal = Calendar.getInstance()
+
+                // 받은 내용으로 Calendar 초기화
+                cal.set(Calendar.YEAR, received.extras!!.getInt("year"))
+                cal.set(Calendar.MONTH, received.extras!!.getInt("month"))
+                cal.set(Calendar.DAY_OF_MONTH, received.extras!!.getInt("dayOfMonth"))
+
+                // 인텐트에 넣어 보낼 데이터 클래스 객체 생성
+                var userInfo = UserInfo(goalDate = cal, d_day = 10, maxCombo = 0, finalMessage = finalMessageInput.text.toString())
+                i.putExtra("userInfo", userInfo)
+                i.putExtra("finalMessage", finalMessageInput.text)
+                i.putExtra("year", received.extras?.getInt("year"))
+                i.putExtra("month", received.extras?.getInt("month"))
+                i.putExtra("dayOfMonth", received.extras?.getInt("dayOfMonth"))
+                startActivity(i)
+            }
         }
     }
 
