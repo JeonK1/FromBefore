5b5a815bd82857498860aa80ccdb17a2451323b9
ImInnocent <ssje2006@naver.com>
2020-08-02 12:55:07 +0900

    편지 쓰기 하드 코딩 수정

--- FromBefore/app/src/main/java/com/example/frombefore/message/WriteMessage.kt
+++ FromBefore/app/src/main/java/com/example/frombefore/message/WriteMessage.kt
@@ -8,6 +8,7 @@ import com.example.frombefore.R
 import com.example.frombefore.manager.UserInfo
 import com.example.frombefore.task.MessageSetterTask
 import kotlinx.android.synthetic.main.activity_write_message.*
+
 class WriteMessage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,27 +18,22 @@ class WriteMessage : AppCompatActivity() {
     }
 
     private fun buttonInit() {
-        btn_send_msg_server2.setOnClickListener {
-            //Todo : send to server
-            finish()
-        }
-        btn_back2.setOnClickListener {
-            finish()
-        }
-    }
-    private fun msgBoxInit() {
+
+        // set button handlers
         msgEditText2.addTextChangedListener(object: TextWatcher {
             override fun afterTextChanged(s: Editable?) {
             }
+
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
             }
+
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
                 val maxWordCnt=100; // 최대입력수
                 val input= msgEditText2.text
-                msgWordCnt2.setText(input.length.toString() + "/" + maxWordCnt)
+                msgWordCnt2.text = input.length.toString() + "/" + maxWordCnt
             }
-
         })
+
         btn_send_msg_server2.setOnClickListener {
             val str = msgEditText2.text.toString()
             val data = MessageData(
@@ -47,8 +43,18 @@ class WriteMessage : AppCompatActivity() {
                 ).toInt(), str
             )
             val code = MessageSetterTask(this).execute(data).get()
+
             finish()
         }
+    }
+
+    private fun msgBoxInit() {
+        // set texts
+        val subject = UserInfo.readFile(this, "subject")
+        write_message_title_top.text = subject + "을(를) 위해 공부하는"
+
+        val dDay = UserInfo.readFile(this, "d_day")
+        write_message_title_top2.text = "D-" + dDay + "의 누군가로부터"//d-n의 누군가로부터
     }
 
 }
