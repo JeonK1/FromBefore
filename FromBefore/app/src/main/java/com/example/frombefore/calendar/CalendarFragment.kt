6283a6d7f56b40d63480def31fc5fc3a0a16d4a8
ImInnocent <ssje2006@naver.com>
2020-08-10 02:34:59 +0900

    쓸데없는 버튼 제거

--- FromBefore/app/src/main/java/com/example/frombefore/calendar/CalendarFragment.kt
+++ FromBefore/app/src/main/java/com/example/frombefore/calendar/CalendarFragment.kt
@@ -123,31 +123,10 @@ class CalendarFragment : Fragment() {
 //        progressBar.setProgress(progressNum)
 //
 //    }
-    private fun initCalendarView() {
 
-        tv_current_day.setText(SimpleDateFormat("M월 dd일", Locale.getDefault()).format(Calendar.getInstance().time))
+    private fun initCalendarView() {
+        tv_current_day.text = SimpleDateFormat("M월 dd일", Locale.getDefault()).format(Calendar.getInstance().time)
 
         CalendarFB(context, tableLayout)
-
-        btn_send_msg.setOnClickListener {
-            //메시지 전송 버튼
-            val secondIntent = Intent(context, MsgSendToMeActivity::class.java)
-            startActivity(secondIntent)
-        }
-
-        if(/* 나에게 온 편지가 있을 때*/1==1){
-            btn_receive_letter.setImageResource(R.drawable.letter_exist)
-            btn_receive_letter.setOnClickListener {
-                //나에게 온 편지가 있는 경우 버튼 클릭 했을 때
-                //TODO : 서버에서 해당 편지 내용 가져와서 읽기
-                val intent = Intent(context, ReceiveMessageActivity::class.java)
-                startActivity(intent)
-            }
-        }
-
-//        else{
-//            //TODO : 읽을 펼지가 없다고 메시지 출력하는데 편지 읽는 dialog랑 형식 맞출꺼라 일단 주석
-//        }
-
     }
 }
